b02144f9e23fe34a0b4b303371a947bfa7abd174
Joshua Chan <joshua.bennett.chan@gmail.com>
2017-11-07 20:07:54 -0600

    version compare fix

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var argv = require('minimist')(process.argv.slice(2));
 console.log(argv);
 
 const instance = new Lib({
-        startVersion: argv.s,
-        endVersion: argv.e
+        startVersion: argv.s.toString(),
+        endVersion: argv.e.toString()
     }, argv.t, argv.o, argv.r)
 
 instance.run();
